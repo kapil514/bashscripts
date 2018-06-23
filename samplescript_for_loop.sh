3aacc943143e6d279f85a6fb716194843adf4ef0
kapil514 <ckapil@live.com>
2018-06-23 22:08:34 +0530

    Create samplescript_for_loop.sh

---
+++ samplescript_for_loop.sh
@@ -0,0 +1,12 @@
+for i in ph283404 ph147504; do grosh -n $i -c"cat /etc/redhat-release";done
+
+
+###############>Kernel Version
+[jwindham@vzsjclsap03 ~](507) $ for i in ph283404 ph147504; do grosh -n $i -c"uname -a";done
+ID sjc40a
+
+
+for i in ph283404 ph147504; do grosh -n $i -c"rpm -qa";done
+
+
+
