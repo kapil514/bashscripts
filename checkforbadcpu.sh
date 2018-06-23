78916f1d42aaa042f29357c3bf7957c73b546f70
kapil514 <ckapil@live.com>
2018-06-23 22:14:39 +0530

    Create checkforbadcpu.sh

---
+++ checkforbadcpu.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+if [ ! -d /var/tmp/badcpu ]; then
+  mkdir /var/tmp/badcpu
+fi
+cd /var/tmp/badcpu
+rm ./*.out ./all
+touch all
+
+SERVERS=$(/opt/totality/bby/bin/gethost -x kvm,ucs,vmw,ps,hpux,sun,vm,project,dell,windows | grep -v dlpolsdb | sort)
+
+for i in $SERVERS; do
+        (ssh -q $i '(echo -n "'$i' " ;echo -n `hpasmcli -s "Show server" |egrep "Processor:|Status" |egrep -B1 "degraded|error|fail"`)' 2>&1 &) > /var/tmp/badcpu/$i.out
+done
+sleep 300
+NOTRESPONDED=`ps -aef |grep hpasmcli |grep -v grep |awk '{print $10}'`
+NOTRESPONDEDPID=`ps -aef |grep hpasmcli |grep -v grep |awk '{print $2}'`
+for i in $NOTRESPONDED
+do
+        echo "$i never responded to the query" >> ./all
+done
+for k in "$NOTRESPONDEDPID"
+do
+        kill -9 $k
+done
+ERROR=`find . -name "*.out" -size 0 |awk -F/ '{print $2}' |awk -F. '{print $1}'`
+for i in $ERROR
+do
+    echo "$i did not return any value" >> ./all
+done
+BROKENHPASMCLI=`egrep -h "not found" *.out |awk '{print $1}'`
+for l in $BROKENHPASMCLI
+do
+        echo "$l has a broken or not existent hpasmcli" >> ./all
+done
+egrep -ih "[0-9] Status" *.out >> ./all
+egrep -i [a-z] ./all 1> /dev/null
+if [ "$?" = "0" ]
+then
+        cat ./all
+fi
